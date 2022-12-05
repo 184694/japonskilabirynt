3382e98c7f2f4f80b247e77b4f1c9e4a8a91f85a
184694 <117123455+184694@users.noreply.github.com>
2022-12-05 20:48:29 +0100

    Add files via upload

--- japonskilabirynt.cs
+++ japonskilabirynt.cs
@@ -2,9 +2,15 @@ namespace JaponskiLabirynt
 {
     public partial class japonskilabirynt : Form
     {
+        private static int WYSOKOSC = 4;
+        private static int SZEROKOSC = 4;
+
+        labirynt LABIRYNT;
+
         public japonskilabirynt()
         {
             InitializeComponent();
+            LABIRYNT = new labirynt(WYSOKOSC, SZEROKOSC);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)

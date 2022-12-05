efaca1219d1417d45649026f4aea3bbc680fc47a
184694 <117123455+184694@users.noreply.github.com>
2022-12-05 20:06:23 +0100

    Add files via upload

---
+++ labirynt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaponskiLabirynt
+{
+
+    internal class labirynt
+    {
+        
+        public static int WYSOKOSC = 4;
+        public static int SZEROKOSC = 4;
+
+        [Flags]
+        public enum KIERUNEK
+        {
+            BRAK = 1 << 0,
+            N = 1 << 1,
+            S = 1 << 2,
+            W = 1 << 3,
+            E = 1 << 4
+        }
+    
+
+        public labirynt(int WYSOKOSC, int SZEROKOSC)
+        {
+            KIERUNEK[,] GRID = new KIERUNEK[WYSOKOSC,SZEROKOSC];
+            wypelnianie(GRID);
+            
+
+        }
+        
+        private void wypelnianie(KIERUNEK[,] GRID)
+        {
+            for (int i = 0; i < WYSOKOSC; i++)
+            {
+                for (int j = 0; j < SZEROKOSC; j++)
+                {
+                    GRID[i, j] = KIERUNEK.N | KIERUNEK.S | KIERUNEK.W | KIERUNEK.E;
+                }
+            }
+        }
+
+
+
+
+    }
+}

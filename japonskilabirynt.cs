f92ad47481494da499d2d595ff0b57d6a15d5343
184694 <117123455+184694@users.noreply.github.com>
2022-12-05 22:36:19 +0100

    Add files via upload

--- japonskilabirynt.cs
+++ japonskilabirynt.cs
@@ -2,8 +2,10 @@ namespace JaponskiLabirynt
 {
     public partial class japonskilabirynt : Form
     {
-        private static int WYSOKOSC = 4;
-        private static int SZEROKOSC = 4;
+        private static int WYSOKOSC = 40;
+        private static int SZEROKOSC = 40;
+
+        private static int[] USTAWIENIA = {2, 5, 20}; // PADDING, PENWIDTH, ROZMIARKOMORKI
 
         labirynt LABIRYNT;
         rysowanie RYSOWANIE;
@@ -12,7 +14,7 @@ namespace JaponskiLabirynt
         {
             InitializeComponent();
             LABIRYNT = new labirynt(WYSOKOSC, SZEROKOSC);
-            RYSOWANIE = new rysowanie(LABIRYNT.GRID);
+            RYSOWANIE = new rysowanie(LABIRYNT.GRID, USTAWIENIA, this);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)

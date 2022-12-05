ad1ba85866c1dbaf04be35ba9a6b254e481dd920
184694 <117123455+184694@users.noreply.github.com>
2022-12-05 14:11:04 +0100

    Add files via upload

---
+++ japonskilabirynt.cs
@@ -0,0 +1,15 @@
+namespace JaponskiLabirynt
+{
+    public partial class japonskilabirynt : Form
+    {
+        public japonskilabirynt()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
